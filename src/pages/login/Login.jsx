d3b71ffeaca57b2e30b368bac2966ae3a6e0fe1f
lucasb-silva <lucasbeserrasilva18@gmail.com>
2024-11-18 07:18:50 -0300

    Estilização da página `<Login />`

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,28 @@ import "./login.scss"
 
 const Login = () => {
   return (
-    <div className="login">Login</div>
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Lets Gather.</h1>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in tempor massa. 
+          Aenean efficitur orci ac tincidunt volutpat. Integer ac ex sodales,
+          tempus ex ac, tempor purus.
+          </p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
