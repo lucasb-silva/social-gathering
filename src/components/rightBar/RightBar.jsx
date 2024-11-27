1fe5677a2133d227c4f5cc0c25c1a79491935578
lucasb-silva <lucasbeserrasilva18@gmail.com>
2024-11-27 04:34:25 -0300

    Modelagem e estilização da `<RightBar \>`

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -2,7 +2,124 @@ import "./rightBar.scss"
 
 const RightBar = () => {
   return (
-    <div className='rightBar'>RightBar</div>
+    <div className='rightBar'>
+      <div className="container">
+        <div className="item">
+          <span>Suggestions For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg" />
+              <span>Jane Doe</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg" />
+              <span>Jane Doe</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg" />
+              <p>
+                <span>Jane Doe</span> changed their cover picture
+              </p>              
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg" />
+              <p>
+                <span>Jane Doe</span> changed their cover picture
+              </p>              
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg" />
+              <p>
+                <span>Jane Doe</span> changed their cover picture
+              </p>              
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg" />
+              <p>
+                <span>Jane Doe</span> changed their cover picture
+              </p>              
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg" />
+              <div className="online" />
+              <span>Jane Doe</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg" />
+              <div className="online" />
+              <span>Jane Doe</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg" />
+              <div className="online" />
+              <span>Jane Doe</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg" />
+              <div className="online" />
+              <span>Jane Doe</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg" />
+              <div className="online" />
+              <span>Jane Doe</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg" />
+              <div className="online" />
+              <span>Jane Doe</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg" />
+              <div className="online" />
+              <span>Jane Doe</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
