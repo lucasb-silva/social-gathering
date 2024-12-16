c69ec8cfec0fb144fd0181c76e512253ebaa02e9
lucasb-silva <lucasbeserrasilva18@gmail.com>
2024-12-16 06:01:40 -0300

    Implementação da seção de comentários

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -5,8 +5,12 @@ import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
+import Comments from "../comments/Comments";
+import { useState } from "react";
 
 const Post = ({ post }) => {
+
+  const [commentOpen, setCommentOpen] = useState(false);
 
   //TEMPORARY
   const liked = false;
@@ -35,7 +39,7 @@ const Post = ({ post }) => {
             {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
             12 Likes
           </div>
-          <div className="item">
+          <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
             <TextsmsOutlinedIcon />
             0 Comments
           </div>
@@ -44,6 +48,7 @@ const Post = ({ post }) => {
             Share
           </div>
         </div>
+        {commentOpen && <Comments />}
       </div>
     </div>
   )
